b1d7379bce3401275e0f4e2b896193d6313b84be
Radhakrishnan Ayyanar <radhakrishnan.ayyanar@tietoevry.com>
2021-12-06 09:54:59 +0530

    Second scenario covered. Allow the Add method to handle an unknown amount of numbers

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 
 public class Calculator {
 
-    private static final String VALID_PATTERN = "^(\\d+|\\d+(,\\d+))$";
+    private static final String VALID_PATTERN_FOR_TWO_NUMBERS = "^(\\d+|\\d+(,\\d+))$";
+    private static final String VALID_PATTERN_FOR_N_NUMBERS = "^(\\d+)?(,\\d+)*$";
 
     /**
      * The method can take up to two numbers, separated by commas, and will return their sum.
+     * And also handle an unknown amount of numbers
      */
     public int add(String numbers) {
 
@@ -16,9 +18,9 @@ public class Calculator {
         }
         int result = 0;
 
-        if (numbers.matches(VALID_PATTERN)) {
-            int[] arr = Arrays.stream(numbers.split(",")).mapToInt(Integer::parseInt).toArray();
-            result = arr.length == 2 ? arr[0] + arr[1] : arr[0];
+        if (numbers.matches(VALID_PATTERN_FOR_TWO_NUMBERS) ||
+                numbers.matches(VALID_PATTERN_FOR_N_NUMBERS)) {
+            result = Arrays.stream(numbers.split(",")).mapToInt(Integer::parseInt).sum();
         } else {
             throw new InvalidInputException("Invalid input");
         }
